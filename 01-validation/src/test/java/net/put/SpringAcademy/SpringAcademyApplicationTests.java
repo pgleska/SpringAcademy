8625c2561bbb9c698d7e2f6909f7cc4a1c722fa9
Piotr Gleska <piotr.gleska@wp.pl>
2022-12-08 12:28:01 +0100

    change structure

---
+++ 01-validation/src/test/java/net/put/SpringAcademy/SpringAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package net.put.SpringAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
